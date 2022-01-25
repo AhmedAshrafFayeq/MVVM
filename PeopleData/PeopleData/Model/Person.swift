24e60947c4f4d9d51237b42a21344df90fd138a4
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-01-25 20:57:04 +0200

    starter view, add model

---
+++ PeopleData/PeopleData/Model/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  PeopleData
+//
+//  Created by Ahmed Fayeq on 25/01/2022.
+//
+
+import Foundation
+
+struct Person {
+    let firstName, lastName, gender: String
+    let height, age: Int
+}
--- PeopleData/PeopleData/ViewController.swift
+++ PeopleData/PeopleData/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = ["Ahmed", "Eslam", "Shifo"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
