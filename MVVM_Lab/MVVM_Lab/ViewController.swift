bc819fe93c0f12630a72a82cae3138a214169ba2
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-01-29 23:15:56 +0200

    edit view

--- MVVM_Lab/MVVM_Lab/ViewController.swift
+++ MVVM_Lab/MVVM_Lab/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var errorlabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var userNameTextFiekd: UITextField!
     @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        errorlabel.isHidden = true
     }
 
     @IBAction func loginButton(_ sender: Any) {
